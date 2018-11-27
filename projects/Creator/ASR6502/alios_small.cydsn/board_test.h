0e25cdc99488f72751085582520d6abc6f2fd6be
Ruilin Hao <ruilinhao@asrmicro.com>
2018-11-27 14:53:08 +0800

    lora: Add a new project for ASR6502
    Signed-off-by: Ruilin Hao <ruilinhao@asrmicro.com>

---
+++ projects/Creator/ASR6502/alios_small.cydsn/board_test.h
@@ -0,0 +1,26 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef _ASR_BOARD_TEST_H
+#define _ASR_BOARD_TEST_H
+#include <stdbool.h>
+#include <stdio.h>
+    
+bool LoRaTestSleep(uint8_t sleep_mode);
+bool LoRaTestMcu(uint8_t mcu_mode);
+bool LoRaTestRx(uint32_t freq, uint8_t dr);
+bool LoRaTestTx(uint32_t freq, uint8_t dr, uint8_t pwr);
+bool LoRaTestTxcw(uint32_t freq, uint8_t pwr, uint8_t opt);
+bool LoRaTestStdby(uint8_t stdby);
+bool loratest_at_process(uint8_t *buf, uint32_t size);
+
+#endif
+/* [] END OF FILE */
